6454eab1f6cc4a71c94ffb450ca2f68c94dcb9ee
FengJitao0930 <84697593+FengJitao0930@users.noreply.github.com>
2023-05-01 05:45:16 +1000

    Add files via upload

--- mastodownCapture.py
+++ mastodownCapture.py
@@ -4,10 +4,11 @@ import couchdb
 import json
 # need to install mastodon.py
 from mastodon import Mastodon, StreamListener
+import re
 
 # connect to the couchdb
-admin = 'jitao'  # change it to your admin account
-password = 'fjt1879774' # change it to your password
+admin = 'cccadmin'  # change it to your admin account
+password = 'whysohard24！' # change it to your password
 
 # send request to couchdb
 url = f'http://{admin}:{password}@127.0.0.1:5984/'
@@ -32,7 +33,19 @@ class Listener(StreamListener):
     def on_update(self, status):
         json_element = json.dumps(status, indent=2, sort_keys=True, default=str)
         json_single = json.loads(json_element)
-        print(json_single['account']['note'])
+
+        # if their account is created in Australia, then store their information
+
+        #ignore the http tag in the sentence, to extract the real words in the note.
+
+        no_tags_string = re.sub('<.*?>', '', json_single["account"]['note'])
+
+        # Replace Unicode line separator and paragraph separator characters
+        no_special_chars_string = no_tags_string.replace(u'\u2028', ' ').replace(u'\u2029', ' ')
+
+        # Replace HTML entities
+        readable_string = no_special_chars_string.replace('&gt;', '>')
+        print(readable_string)
         doc_id, doc_rev = db.save(json_single)
         print(f'Document saved with ID:{doc_id} and revision: {doc_rev}')
 
