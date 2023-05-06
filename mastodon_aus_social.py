78db888f03856c519dbf3c4aa8c55bdceb438cb6
FengJitao0930 <84697593+FengJitao0930@users.noreply.github.com>
2023-05-06 22:57:47 +1000

    Add files via upload

--- mastodon_aus_social.py
+++ mastodon_aus_social.py
@@ -15,7 +15,7 @@ url = f'http://{admin}:{password}@172.26.135.17:5984/'
 couch = couchdb.Server(url)
 
 # create a couchdb database called 'mastodon_au_social', if the database exists,just find that database. If not, just creat the database.
-db_name = 'mastodon_au_social'
+db_name = 'mastodon_au_social_final'
 if db_name not in couch:
     db = couch.create(db_name)
 else:
