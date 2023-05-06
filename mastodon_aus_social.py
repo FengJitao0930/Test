52d18ab103c11d898fd9a5d77807966c0c21aa2a
FengJitao0930 <84697593+FengJitao0930@users.noreply.github.com>
2023-05-06 22:45:23 +1000

    Add files via upload

--- mastodon_aus_social.py
+++ mastodon_aus_social.py
@@ -11,11 +11,17 @@ admin = 'cccadmin'
 password = 'whysohard24!'
 
 # send request to couchdb
-url = f'http://{admin}:{password}@127.0.0.1:5984/'
+url = f'http://{admin}:{password}@172.26.135.17:5984/'
 couch = couchdb.Server(url)
 
 # create a couchdb database called 'mastodon_au_social', if the database exists,just find that database. If not, just creat the database.
-db_name = 'mastodon_au_social_final'
+db_name = 'mastodon_au_social'
+if db_name not in couch:
+    db = couch.create(db_name)
+else:
+    db = couch[db_name]
+
+
 m = Mastodon(
     api_base_url='https://aus.social',
     access_token='2nwIHJnHrDkZkFnzbFEgDZmsYTTgu8aXnutLRRgermg'
@@ -26,31 +32,30 @@ m = Mastodon(
 class Listener(StreamListener):
     def on_update(self, status):
         # if some message have some error, just print error to let the script run
-        try:
-            json_element = json.dumps(status, indent=2, sort_keys=True, default=str)
-            json_single = json.loads(json_element)
+
+        json_element = json.dumps(status, indent=2, sort_keys=True, default=str)
+        json_single = json.loads(json_element)
 
 
 
 
-            #ignore the http tag in the sentence, to extract the real words in the note.
+        #ignore the http tag in the sentence, to extract the real words in the note.
 
-            no_tags_string = re.sub('<.*?>', '', json_single['content'])
+        no_tags_string = re.sub('<.*?>', '', json_single['content'])
 
-            # Replace Unicode line separator and paragraph separator characters
-            no_special_chars_string = no_tags_string.replace(u'\u2028', ' ').replace(u'\u2029', ' ')
+        # Replace Unicode line separator and paragraph separator characters
+        no_special_chars_string = no_tags_string.replace(u'\u2028', ' ').replace(u'\u2029', ' ')
 
-            # Replace HTML entities
-            readable_string = no_special_chars_string.replace('&gt;', '>')
+        # Replace HTML entities
+        readable_string = no_special_chars_string.replace('&gt;', '>')
 
-            # create a new dictionary to store the capture data
-            new_store = {}
-            new_store['id'] = json_single['account']['id']
-            new_store['content'] = readable_string
-            new_store['created_at'] = json_single['created_at']
-            doc_id, doc_rev = db.save(new_store)
-        except:
-            print("error")
+        # create a new dictionary to store the capture data
+        new_store = {}
+        new_store['id'] = json_single['account']['id']
+        new_store['content'] = readable_string
+        new_store['created_at'] = json_single['created_at']
+        doc_id, doc_rev = db.save(new_store)
+
 
 
 # open the harvest
